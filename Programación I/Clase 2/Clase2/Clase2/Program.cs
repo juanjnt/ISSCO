58b81d8e13eeff5cc23545fa0f717234dc33e9d6
juanjnt <59158735+juanjnt@users.noreply.github.com>
2022-08-19 09:53:38 -0300

    1.0.0
    Initial version

---
+++ Programación I/Clase 2/Clase2/Clase2/Program.cs
@@ -0,0 +1,27 @@
+﻿//Ejercicio 1
+/*Console.WriteLine("Ingrese un nombre");
+string nombre = Console.ReadLine();
+string resultado = "Este es un programa de " + nombre;
+Console.WriteLine(resultado);*/
+
+
+Console.WriteLine("Ingrese un número decimal");
+string lectura1 = Console.ReadLine();
+decimal variable1; //= Convert.ToDecimal(lectura1);
+
+bool resultad = decimal.TryParse(lectura1, out variable1);
+if (!resultad)
+{
+    Console.WriteLine("El valor no es correcto, se utilizara 0");
+}
+//decimal.Parse(lectura1);
+Console.WriteLine("Ingrese un número entero" );
+int variable2 = Convert.ToInt32(Console.ReadLine());
+decimal variable3 = variable1 + variable2;
+
+Console.WriteLine($"La suma de {variable1}  y {variable2} da como resultado {variable3}");
+
+
+
+
+
