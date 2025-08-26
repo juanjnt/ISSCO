d70e34fbde281f96fae6029023d4f45a3878d05d
- <59158735+juanjnt@users.noreply.github.com>
2025-08-26 19:21:51 -0300

    1.0.0
    Initial version

---
+++ Programación I/Nueva carpeta/Clase2/Clase2/Persona.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase2
+{
+    public class Persona
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public DateTime Nacimiento { get; set; }
+        public int Dni { get; set; }
+        public double Peso { get; set; }
+        public double Altura { get; set; }
+    }
+}
