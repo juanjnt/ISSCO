68a67b0ebaf61c1f2cdd4106ea4a48bc8e4fb142
- <59158735+juanjnt@users.noreply.github.com>
2025-10-17 21:11:00 -0300

    1.0.0
    Initial version

---
+++ Programación I/Clase13/Clase13/Program.cs
@@ -0,0 +1,163 @@
+﻿/*Crea una clase Producto con atributos como nombre y precio.
+Permite agregar varias instancias en una lista.
+Calcula el total del inventario y busca el producto más caro y el más barato.
+Muestra toda la lista con sus precios y el total.*/
+
+using Clase13;
+using System.Data;
+
+/*List<Producto> listaProducto = new List<Producto>();
+
+
+while (true)
+{
+    Console.WriteLine("Ingresar un nuevo producto? 1-Si / 2-No");
+	if (Console.ReadLine() == "1")
+	{
+		Console.WriteLine("Ingrese el nombre");
+		string nombre = ValidacionString();
+		Console.WriteLine("Ingrese el precio");
+		double precio = ValidacionDouble();
+		listaProducto.Add(new Producto() { Nombre = nombre, Precio = precio });
+    }
+	else
+		break;
+}
+Resultados();
+
+void Resultados()
+{
+	Console.WriteLine("Lista de Precios:");
+	double  total = 0;
+	var max = new Producto();
+	var min = new Producto();
+	for (int i = 0; i < listaProducto.Count; i++)
+	{
+        var row = listaProducto[i];
+		Console.WriteLine($"{row.Nombre}: {row.Precio:C2}");
+		
+		if (i == 0)
+		{
+			max = row;
+			min = row;
+		}
+		total += row.Precio;
+		if (row.Precio > max.Precio)
+			max = row;
+		if (row.Precio < min.Precio)
+			min = row;
+	}
+	Console.WriteLine("--------------");
+    Console.WriteLine($"Importe total: {total:C2}");
+    Console.WriteLine($"Producto con precio más alto -> {max.Nombre}: {max.Precio:C2}");
+    Console.WriteLine($"Producto con precio más bajo -> {min.Nombre}: {min.Precio:C2}");
+
+}
+
+
+string ValidacionString()
+{
+	string nombre = string.Empty;
+    while (true)
+	{
+        nombre = Console.ReadLine();
+		if (string.IsNullOrWhiteSpace(nombre))
+            Console.WriteLine("Error en el valor ingresado");
+		else
+			break;
+	}
+	return nombre;
+}
+
+double ValidacionDouble()
+{
+	double valor = 0;
+	while (!double.TryParse(Console.ReadLine(), out valor))
+	{
+		Console.WriteLine("Valor incorrecto");
+	}
+	return valor;
+}
+
+int ValidacionEntero()
+{
+    int valor = 0;
+    while (!int.TryParse(Console.ReadLine(), out valor))
+    {
+        Console.WriteLine("Valor incorrecto");
+    }
+    return valor;
+}*/
+/*Crear una clase Ciudad con atributos: nombre y temperatura en °C. 
+ * Permitir ingresar varias ciudades. Al terminar, mostrar:
+Listado de todas las ciudades y su temperatura.
+La ciudad con la temperatura más alta y la más baja.
+La temperatura promedio de todas las ciudades.*/
+
+List<Ciudad> listaCiudad = new List<Ciudad>();
+while (true)
+{
+    Console.WriteLine("Ingresar un nueva ciudad? 1-Si / 2-No");
+    if (Console.ReadLine() == "1")
+    {
+        Console.WriteLine("Ingrese el nombre");
+        string nombre = ValidacionString();
+        Console.WriteLine("Ingrese la temperatura");
+        double temperatura = ValidacionDouble();
+        listaCiudad.Add(new Ciudad() { Nombre = nombre, Temperatura = temperatura });
+    }
+    else
+        break;
+}
+Resultados();
+void Resultados()
+{
+    double total = 0;
+    var max = new Ciudad();
+    var min = new Ciudad();
+    for (int i = 0; i < listaCiudad.Count; i++)
+    {
+        var row = listaCiudad[i];
+
+        if (i == 0)
+        {
+            max = row;
+            min = row;
+        }
+        total += row.Temperatura;
+        if (row.Temperatura > max.Temperatura)
+            max = row;
+        if (row.Temperatura < min.Temperatura)
+            min = row;
+    }
+    total = total / listaCiudad.Count;
+    Console.WriteLine("--------------");
+    Console.WriteLine($"Promedio de temperaturas: {total:N2}º");
+    Console.WriteLine($"Ciudad con la temperatura más alta -> {max.Nombre}: {max.Temperatura:N2}º");
+    Console.WriteLine($"Ciudad con la temperatura más baja -> {min.Nombre}: {min.Temperatura:N2}º");
+}
+
+
+double ValidacionDouble()
+{
+    double valor = 0;
+    while (!double.TryParse(Console.ReadLine(), out valor))
+    {
+        Console.WriteLine("Valor incorrecto");
+    }
+    return valor;
+}
+
+string ValidacionString()
+{
+    string nombre = string.Empty;
+    while (true)
+    {
+        nombre = Console.ReadLine();
+        if (string.IsNullOrWhiteSpace(nombre))
+            Console.WriteLine("Error en el valor ingresado");
+        else
+            break;
+    }
+    return nombre;
+}
---
+++ Programación I/Clase9/Clase9/Clase9/Program.cs
@@ -0,0 +1,171 @@
+﻿/*
+4- Un banco implemente un sistema de inversiones, 
+el usuario necesita saber cuales son las ganancia de acuerdo a las opciones, 
+se obtiene un interes del 35% anual a entre 30 y 59 días, 
+un interes del 40% anual entre 60 y 89 días y para 90 o más 43% anual.
+El usuario ingresa el monto a invertir en pesos.
+Calcular cuánto tendrá al final de un año si se capitaliza anualmente.
+Mostrar monto final en pesos argentinos.
+ 
+ */
+
+
+using System.Diagnostics;
+
+double importe = 0, tasa = 0,  pago = 0;
+
+
+void DatosIniciales()
+{
+    while (importe == 0)
+    {
+        Console.WriteLine("Ingrese el importe a invertir");
+        importe = ValidarNumero(Console.ReadLine());
+    }
+    while (tasa == 0)
+    {
+        Console.WriteLine("Ingrese la tasa de interes");
+        tasa = ValidarNumero(Console.ReadLine());
+    }
+}
+List<string> menu = new List<string>() { "Simular", "Invertir", "Salir" };
+MenuListaValidado(menu);
+MenuPrincipal();
+Console.WriteLine($"Total pagado: ${pago.ToString("N2")}");
+void MenuPrincipal()
+{
+    Console.Clear();
+    List<string> menu = new List<string>() { "Simular", "Invertir", "Salir" };
+    int respuesta = MenuLista(menu);
+    switch (respuesta)
+    {
+        case 1:
+            MenuCalculo(true);
+            break;
+        case 2:
+            MenuCalculo(false);
+            break;
+        default:
+            break;
+    }
+}
+void ResultadoCalculo(int respuesta, int dias,  double importe, bool simular)
+{
+    switch (respuesta)
+    {
+        case 1:
+            importe = CalculoDeInversion(dias, 35, importe);
+            break;
+        case 2:
+            importe = CalculoDeInversion(dias, 40, importe);
+            break;
+        case 3:
+            importe = CalculoDeInversion(dias, 43, importe);
+            break;
+        case 4:
+            MenuPrincipal();
+            break;
+        default:
+            break;
+    }
+    if (!simular)
+    {
+        pago += importe;
+    }
+    Console.WriteLine("[Enter para continuar]");
+    Console.ReadLine();
+    MenuPrincipal();
+
+
+}
+void MenuCalculo(bool simular)
+{
+    Console.Clear();
+    List<string> menu = new List<string>() { "35% 30 a 59 días", "40% 60 a 89 días", "43% más 90 días", "Volver" };
+    int respuesta = MenuLista(menu);
+    int diasCalculo = 0;
+    while (diasCalculo == 0)
+    {
+        Console.WriteLine("Ingrese los días a invertir");
+        diasCalculo = ValidarEntero(Console.ReadLine());
+    }
+    double importeInvertir = 0;
+    while (importeInvertir == 0)
+    {
+        Console.WriteLine("Ingrese el importe a invertir");
+        importeInvertir = ValidarNumero(Console.ReadLine());
+    }
+
+    ResultadoCalculo(respuesta, diasCalculo, importeInvertir, simular);
+}
+double CalculoDeInversion(int dias, double porcentaje, double importe)
+{
+    double tasa = (porcentaje / 365.0) * (double)dias;
+    double ganancia = (tasa * importe) / 100;
+    Console.WriteLine($"Ganancia calculada: {ganancia.ToString("N2")}");
+    return ganancia;
+}
+int MenuLista(List<string> lista)
+{
+    for (int i = 0; i < lista.Count; i++)
+    {
+        Console.WriteLine($"{i+1}- {lista[i]}");
+    }
+    int valor = 0;
+    while (valor == 0)
+    {
+        Console.WriteLine("Seleccione una opción");
+        valor = ValidarEntero(Console.ReadLine());
+    }
+    return valor;
+}
+double ValidarNumero(string valor)
+{
+    double num;
+    if (!double.TryParse(valor, out num))
+    {
+        Console.Clear();
+        Console.WriteLine("Error al convertir el número");
+        return num;
+    }
+    return num;
+}
+int ValidarEntero(string valor)
+{
+    int num;
+    if (!int.TryParse(valor, out num))
+    {
+        Console.WriteLine("Error al convertir el número");
+        return num;
+    }
+    return num;
+}
+
+
+void MenuListaValidado(List<string> lista)
+{
+    for (int i = 0; i < lista.Count; i++)
+    {
+        Console.WriteLine($"{i + 1}- {lista[i]}");
+    }
+    int valor = 0;
+    if (!int.TryParse(Console.ReadLine(), out valor))
+    {
+        MenuListaValidado(lista);
+    }
+    switch (valor)
+    {
+        case 1:
+            Console.WriteLine("Opción 1");
+            break;
+        case 2:
+            Console.WriteLine("Opción 2");
+            break;
+        case 3:
+            Console.WriteLine("Opción 3");
+            break;
+        default:
+            MenuListaValidado(lista);
+            break;
+    }
+}
