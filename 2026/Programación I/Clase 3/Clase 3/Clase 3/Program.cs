0667302cb7c93b358adb926a92375529c1b22e60
- <59158735+juanjnt@users.noreply.github.com>
2026-08-14 21:30:29 -0300

    1.0.0

--- 2026/Programación I/Clase 3/Clase 3/Clase 3/Program.cs
+++ 2026/Programación I/Clase 3/Clase 3/Clase 3/Program.cs
@@ -39,7 +39,7 @@ Console.WriteLine($"El sueldo neto promedio es de {totalSueldo:C2}");*/
  * Mostrar el total a pagar por cada factura y, al final del programa, 
  * mostrar la suma total recaudada por concepto de recargos.*/
 
-double totalRecargo = 0;
+/*double totalRecargo = 0;
 for (int i = 0; i < 5; i++)
 {
     double recargo = 0;
@@ -63,4 +63,60 @@ for (int i = 0; i < 5; i++)
     Console.WriteLine("------------------------------------------");
 }
 
-Console.WriteLine($"Total recaudado en concepto de Recargo: {totalRecargo:C2}");
+Console.WriteLine($"Total recaudado en concepto de Recargo: {totalRecargo:C2}");*/
+
+/*Pedir al usuario la cantidad total de turnos de trabajo a evaluar. Ingresar la 
+ * cantidad de unidades producidas y la cantidad de unidades defectuosas de ese turno. 
+ * Se debe calcular e informar el total de unidades aptas de ese turno (producidas menos defectuosas). 
+ * Al finalizar todos los turnos, calcular el porcentaje global de producción defectuosa sobre la producción total.*/
+
+/*Console.WriteLine("Ingrese la cantidad de turnos a evaluar");
+int turnos = int.Parse(Console.ReadLine());
+int producidas = 0;
+int defectuosas = 0;
+for (int i = 0; i < turnos; i++)
+{
+    Console.WriteLine("Ingrese la cantidad de unidades producidas");
+    int producidasTurno = int.Parse(Console.ReadLine());
+    Console.WriteLine("Ingrese la cantidad de unidades defectuosas");
+    int defectuosasTurno = int.Parse(Console.ReadLine());
+    int aptas = producidasTurno - defectuosasTurno;
+    Console.WriteLine($"Total de unidades aptas en turno: {aptas}");
+
+    producidas += producidasTurno;
+    defectuosas += defectuosasTurno;
+}
+
+double porcentaje = (double)defectuosas * 100 / (double)producidas;
+Console.WriteLine($"Porcentaje de producción total defectuosa: {porcentaje:N2}%");*/
+
+/*Pedir el monto inicial de un crédito y la cantidad fija de meses a financiar. 
+ * Simular mes a mes el avance de la deuda aplicando un interés mensual del 3% 
+ * sobre el saldo acumulado del mes anterior. En cada mes, mostrar el número de mes, 
+ * el monto del interés generado y el nuevo saldo total. Al finalizar, indicar cuánto 
+ * fue el monto total pagado en concepto puro de intereses.*/
+
+/*double montoInicial = 0;
+Console.WriteLine("Ingrese el monto inicial a financiar");
+montoInicial = double.Parse(Console.ReadLine());
+int meses = 0;
+Console.WriteLine("Ingrese la cantidad de meses a financiar");
+meses = int.Parse(Console.ReadLine());
+const double tasa = 0.03;
+
+double totalAcumulado = montoInicial;
+double interesAcumulado = 0;
+for (int i = 0; i < meses; i++)
+{
+    double interesMensual = totalAcumulado * tasa;
+    interesAcumulado += interesMensual;
+    totalAcumulado += interesMensual;
+    Console.WriteLine("===================");
+    Console.WriteLine($"Resumen del mes {i + 1}");
+    Console.WriteLine("===================");
+    Console.WriteLine($"Interes mensual----{interesMensual:C2}");
+    Console.WriteLine($"Saldo Acumulado----{totalAcumulado:C2}");
+    Console.WriteLine("===================");
+}
+
+Console.WriteLine($"Concepto puro de interes-----{interesAcumulado:C2}");*/
---
+++ 2026/Programación I/Clase 4/Clase 4/Clase 4/Program.cs
@@ -0,0 +1,76 @@
+﻿
+/*
+    Console.WriteLine("Que es usted?");
+
+    int valor = 0;
+    valor = int.Parse(Console.ReadLine());
+
+    switch (valor)
+    {
+        case 0:
+            Console.WriteLine("Soy un cero");
+            break;
+        case 1:
+            Console.WriteLine("Soy un uno");
+            break;
+        case 2:
+            Console.WriteLine("Soy un dos");
+            break;
+        default:
+            Console.WriteLine("No soy nada");
+            break;
+    }
+
+
+
+
+    if (valor == 0)
+    {
+        Console.WriteLine("Soy un cero");
+    }
+    else if (valor == 1)
+    {
+        Console.WriteLine("Soy un uno");
+    }
+    else if (valor == 2)
+    {
+        Console.WriteLine("Soy un dos");
+    }
+    else
+    {
+        Console.WriteLine("No soy nada");
+    }
+*/
+
+while (true)
+{
+
+
+
+    Console.WriteLine("Ingrese el tipo de vehículo [MOTO, AUTO, CAMIONETA]");
+    string vehiculo = Console.ReadLine().ToUpper();
+    double precio = 0;
+    switch (vehiculo)
+    {
+        case "MOTO":
+            precio = 800;
+            break;
+        case "AUTO":
+            precio = 1500;
+            break;
+        case "CAMIONETA":
+            precio = 2200;
+            break;
+        default:
+            Console.WriteLine("Tipo de vehículo no reconocido");
+            break;
+    }
+    if (precio > 0)
+    {
+        Console.WriteLine("Ingrese la cantidad de horas de estacionamiento");
+        int horas = int.Parse(Console.ReadLine());
+
+        double total = precio * (double)horas;
+        Console.WriteLine($"Importe a pagar: {total:C2}");
+    }
+}
